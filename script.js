30b5257165fbf57f403e1ca434b667863730110f
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-25 12:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //your JS code here. If required.
 function enter() {
 	let p = document.getElementById("status");
-	// let pText = p.innerText;
-	let h1 = document.createElement("h1");
-	h1.id = "status";
-	h1.innerText = 'Entered Metaverse';
-	p.remove();
-	let body = document.querySelector("body");
-	body.appendChild(h1);
+	p.innerHTML = '<h1>Entered Metaverse</h1>'
+	// // let pText = p.innerText;
+	// let h1 = document.createElement("h1");
+	// h1.id = "status";
+	// h1.innerText = 'Entered Metaverse';
+	// p.remove();
+	// let body = document.querySelector("body");
+	// body.appendChild(h1);
 }
 // let btn = document.getElementById("enterBtn");
 // btn.addEventListener("click", enter());
