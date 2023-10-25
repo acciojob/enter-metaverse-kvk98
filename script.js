81bea5ee49dd35427f34c87c93f15e74ef8f3599
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-25 11:43:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function enter() {
+	let p = document.getElementById("status");
+	let pText = p.innerText;
+	let h1 = document.createElement("h1");
+	h1.innerText = "Entered Metaverse";
+	p.remove();
+	let body = document.querySelector("body");
+	body.appendChild(h1);
+}
+// let btn = document.getElementById("enterBtn");
+// btn.addEventListener("click", enter());
